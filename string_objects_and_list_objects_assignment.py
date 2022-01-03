643adb2d9768969d6bc01cafb2e7556dd2a1749e
arshnoorkhan23 <93009311+arshnoorkhan23@users.noreply.github.com>
2022-01-03 10:28:09 +0530

    Add files via upload

--- string_objects_and_list_objects_assignment.py
+++ string_objects_and_list_objects_assignment.py
@@ -11,7 +11,12 @@
 # question number 1
 
 n=9   # nos of rows
-r=  n//2+1   # rows after which inflection occurs
+#r=  n//2+1   # rows after which inflection occurs
+
+if n%2==0:
+    r=n/2
+else:
+    r=n//2+1
 for i in range(1,n+2):
     if i<=r:
         for j in range(0,i):
