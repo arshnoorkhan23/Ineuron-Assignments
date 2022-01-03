7f148dacca61ce156845c7453fa9b473209e4830
arshnoorkhan23 <93009311+arshnoorkhan23@users.noreply.github.com>
2022-01-03 10:23:54 +0530

    Add files via upload

---
+++ string_objects_and_list_objects_assignment.py
@@ -0,0 +1,32 @@
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
+
+# question number 1
+
+n=9   # nos of rows
+r=  n//2+1   # rows after which inflection occurs
+for i in range(1,n+2):
+    if i<=r:
+        for j in range(0,i):
+            print('*',end='')
+        print('\n')
+    else:
+        for j in range(n-i+1,0,-1):
+            print('*',end='')
+        print('\n')
+
+
+######################################
+
+# question number 2
+name=str(input('Enter the name'))
+print(name)
+rev_name=name[::-1]
+print(rev_name)
